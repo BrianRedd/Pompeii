41cf722b2482324c44a3f32e6e3f6e80c9111dcd
Brian Redd <bredd@trinidadbenham.com>
2020-02-19 21:04:05 -0700

    added grid spaces to cards

--- src/data/data.js
+++ src/data/data.js
@@ -6,77 +6,88 @@ export const cardsData = {
     type: "Pomp",
     number: 1,
     color: "G",
-    count: 4
+    count: 4,
+    grid: ["0-1", "1-1"]
   },
   CARD_2: {
     name: "Purple 2",
     type: "Pomp",
     number: 2,
     color: "P",
-    count: 4
+    count: 4,
+    grid: ["4-0", "4-1"]
   },
   CARD_3: {
     name: "Purple 3",
     type: "Pomp",
     number: 3,
     color: "P",
-    count: 5
+    count: 5,
+    grid: ["2-2", "3-2"]
   },
   CARD_4: {
     name: "Grey 4",
     type: "Pomp",
     number: 4,
     color: "G",
-    count: 7
+    count: 7,
+    grid: ["5-2", "6-2"]
   },
   CARD_5: {
     name: "Purple 5",
     type: "Pomp",
     number: 5,
     color: "P",
-    count: 4
+    count: 4,
+    grid: ["8-2"]
   },
   CARD_6: {
     name: "Turquoise 6",
     type: "Pomp",
     number: 6,
     color: "T",
-    count: 7
+    count: 7,
+    grid: ["2-3", "3-3", "2-4", "3-4"]
   },
   CARD_7: {
     name: "Purple 7",
     type: "Pomp",
     number: 7,
     color: "P",
-    count: 3
+    count: 3,
+    grid: ["5-4"]
   },
   CARD_8: {
     name: "Grey 8",
     type: "Pomp",
     number: 8,
     color: "G",
-    count: 5
+    count: 5,
+    grid: ["6-3", "6-4"]
   },
   CARD_9: {
     name: "Turquoise 9",
     type: "Pomp",
     number: 9,
     color: "T",
-    count: 4
+    count: 4,
+    grid: ["4-5"]
   },
   CARD_10: {
     name: "Brown 10",
     type: "Pomp",
     number: 10,
     color: "B",
-    count: 5
+    count: 5,
+    grid: ["5-5", "5-6"]
   },
   CARD_11: {
     name: "Brown 11",
     type: "Pomp",
     number: 11,
     color: "B",
-    count: 5
+    count: 5,
+    grid: ["8-5", "8-6", "9-5", "10-5", "10-6"]
   },
   OMEN: {
     name: "Omen",
